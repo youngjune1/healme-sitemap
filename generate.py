1cb969e7a1e4632fc3681b37f6148494370f42b1
youngjune1 <zelsen7@naver.com>
2025-05-14 01:21:26 +0900

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://healme.kr"
+MAX_POSTS = 1500
+STEP = 150
+urls = []
+
+for start in range(1, MAX_POSTS, STEP):
+    feed_url = f"{BASE_URL}/atom.xml?redirect=false&start-index={start}&max-results={STEP}"
+    res = requests.get(feed_url)
+    soup = BeautifulSoup(res.content, "xml")
+    entries = soup.find_all("entry")
+    for entry in entries:
+        url = entry.find("link")["href"]
+        urls.append(url)
+
+with open("sitemap.xml", "w", encoding="utf-8") as f:
+    f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+    for u in urls:
+        f.write(f"  <url><loc>{u}</loc></url>\n")
+    f.write('</urlset>')
